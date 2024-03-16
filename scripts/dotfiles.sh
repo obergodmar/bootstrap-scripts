53e34cc698c1767de1ca57ff810aa76f7f2f6cce
Vitaly (wsl) <obergodmar@gmail.com>
2024-03-16 23:18:45 +0300

    fix: update dotfiles

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 clone_dotfiles() {
-  if [[ -d "$HOME/dotfiles" ]]; then
-    if git -C "$HOME/dotfiles" pull; then
+  local dotfiles="$HOME/dotfiles"
+
+  if [[ -d "$dotfiles" ]]; then
+    if git -C "$dotfiles" reset HEAD && git -C "$dotfiles" checkout -- .; then
+      display_message "dotfiles has been reset"
+    else
+      display_message "could not reset dotfiles"
+    fi
+
+    if git -C "$dotfiles" pull; then
       display_message "dotfiles has been updated"
     else
       display_error "could not update dotfiles"
@@ -10,9 +18,9 @@ clone_dotfiles() {
       return 1
     fi
   else
-    REPO=https://github.com/obergodmar/dotfiles.git
+    local repo=https://github.com/obergodmar/dotfiles.git
 
-    if git clone $REPO "$HOME/dotfiles"; then
+    if git clone $repo "$dotfiles"; then
       display_message "dotfiles has been cloned"
     else
       display_error "could not clone dotfiles"
