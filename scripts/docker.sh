cbe882fef1f45d13eabac19563cfaea82f44bea0
Vitaly <obergodmar@gmail.com>
2025-03-22 02:00:45 +0300

    fix: correct condition

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -20,7 +20,7 @@ install_docker() {
     local prepare_docker_key="sudo chmod a+r /etc/apt/keyrings/docker.asc"
     local apt_repository="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable"
 
-    if [[ -f "/etc/apt/keyrings/docker.asc" ]]; then
+    if ! [[ -f "/etc/apt/keyrings/docker.asc" ]]; then
         if prepare_keyrings && fetch_gpg_key && prepare_docker_key && echo "$apt_repository" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null; then
             display_message "Docker repository is successfully added"
         else
