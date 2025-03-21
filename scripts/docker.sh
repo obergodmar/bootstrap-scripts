1fdcbd88c76adb873df2c1bfceced505b84697a3
Vitaly <obergodmar@gmail.com>
2025-03-22 02:04:47 +0300

    fix: refactor docker installation commands

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -15,13 +15,13 @@ install_docker() {
 
     display_message "Setting docker repository..."
 
-    local prepare_keyrings="sudo install -m 0754 -d /etc/apt/keyrings"
-    local fetch_gpg_key="sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc"
-    local prepare_docker_key="sudo chmod a+r /etc/apt/keyrings/docker.asc"
-    local apt_repository="deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable"
-
     if ! [[ -f "/etc/apt/keyrings/docker.asc" ]]; then
-        if "$prepare_keyrings" && "$fetch_gpg_key" && "$prepare_docker_key" && echo "$apt_repository" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null; then
+        sudo install -m 0754 -d /etc/apt/keyrings &&
+            sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
+            sudo chmod a+r /etc/apt/keyrings/docker.asc &&
+            echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+        if [[ $? -eq 0 ]]; then
             display_message "Docker repository is successfully added"
         else
             display_error "Could not add docker repository"
@@ -29,7 +29,6 @@ install_docker() {
         fi
     else
         display_warning "Docker repository is already added"
-
         return
     fi
 
