0e1125d62ba9c04da3358d58c2b9ad20419b5a5b
Vitaly <obergodmar@gmail.com>
2025-03-22 02:15:45 +0300

    feat: refactor

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -16,10 +16,13 @@ install_docker() {
     display_message "Setting docker repository..."
 
     if ! [[ -f "/etc/apt/keyrings/docker.asc" ]]; then
-        sudo install -m 0754 -d /etc/apt/keyrings &&
+        sudo install -m 0755 -d /etc/apt/keyrings &&
             sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
             sudo chmod a+r /etc/apt/keyrings/docker.asc &&
-            echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+            echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+            sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
         if [[ $? -eq 0 ]]; then
             display_message "Docker repository is successfully added"
@@ -29,7 +32,6 @@ install_docker() {
         fi
     else
         display_warning "Docker repository is already added"
-        return
     fi
 
     update
